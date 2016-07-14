8846019f3ca606c574f0da7db7175eba1aa9a474
root <root@silkie.cfht.hawaii.edu>
2016-07-14 13:57:46 -1000

    LAMP stack script ready

--- lamp.sh
+++ lamp.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
 
 # install lamp stack and other php libraries
-#sudo yum clean all;
-#sudo yum update;
-#sudo yum -y install httpd mysql php php-mysql php-pdo php-mbstring php-curl;
+echo "Installing apache (httpd)..."
+echo ""
+sudo yum -y install httpd
+sudo systemctl enable httpd.service
+sudo systemctl start httpd.service
+
+echo "Installing MySQL..."
+echo ""
+sudo yum -y install mariadb mariadb-server
+sudo systemctl enable mariadb.service
+sudo systemctl start mariadb.service
+
+echo "Installing PHP libraries..."
+echo ""
+sudo yum -y install php php-mysql php-mcrypt php-pdo php-mbstring php-curl
 
 # import config files from snipeit and httpd
 # ...
@@ -21,6 +33,7 @@
 # ...
 
 # import database (y/n) 
+echo ""
 echo Would you like to import another database [y/n]?	
 read input
 while [ $input != "y" ] && [ $input != "Y" ] && [ $input != "n" ] && [ $input != "N" ]
@@ -30,22 +43,29 @@ do
 done
 if [[ $input = "y" ||  $input = "Y" ]]
 then
-	# After importing Snipe-IT files, locate MySQL backups
+	echo "work in progress"
 elif [[ $input = "n" ||  $input = "N" ]];
 then
-	echo "Change MySQL root password (y/n)?"
+	echo "Change MySQL root password [y/n]?"
 	read input
 	if [[ $input = "n" || $input = "N" ]]; then
 		ticket=1
 		echo "Please enter the NAME of the new database!"
 		read dbname
-		echo "Please enter password for root"
+		echo "Enter root password:"
 		read rootpasswd
 		echo "Creating new database..."
 		mysql -u root -p${rootpasswd} -e "CREATE DATABASE ${dbname};"
+		if [ "$?" -eq 0 ];
+		then ticket=0
+		else ticket=1
+		fi
 		while [ $ticket != 0 ]
 		do
-			echo "Login failed, please try again :("
+			echo ""
+			echo "Login failed, please try again"
+			echo ""
+			echo "Enter root password:"
 			read rootpasswd
 			mysql -u root -p${rootpasswd} -e "CREATE DATABASE ${dbname};"
 			if [ "$?" -eq 0 ];
@@ -53,6 +73,7 @@ then
 			else ticket=1
 			fi
 		done
+		echo ""
 		echo "Database successfully created!"
 		echo "Showing existing databases..."
 		mysql -u root -p${rootpasswd} -e "show databases;"
@@ -61,25 +82,45 @@ then
 		else ticket=1
 		fi
 		echo ""
-		echo "You're good now :)"
 
 	else
-		echo "Enter current password"
+		ticket=1
+		echo "Enter current password:"
 		read oldpasswd
-		echo "Enter new password"
+		echo "Enter new password:"
 		read rootpasswd
 		echo "Changing root password..."
 		mysqladmin -u root -p${oldpasswd} password ${rootpasswd}
-		echo MySQL root password has been set to $rootpasswd
+		if [ "$?" -eq 0  ];
+		then ticket=0
+		else ticket=1
+		fi
+		while [ $ticket != 0 ]
+		do
+			echo ""
+			echo "Login Failed, please try again"
+			echo "Enter current password:"
+			read oldpasswd
+			echo "Enter new password:"
+			read rootpasswd
+			mysqladmin -u root -p${oldpasswd} password ${rootpasswd}
+			if [ "$?" -eq 0 ];
+			then ticket=0
+			else ticket=1
+			fi
+		done
+		echo ""
+		echo Success! MySQL root password has been set to $rootpasswd
 		echo "Please enter the NAME of the new database!"
 		read dbname
 		echo "Creating new database..."
-		mysql -u root -p -e "CREATE DATABASE ${dbname};"
-		echo "Database successfully created!"
-		echo "Showing existing databases..."
-		mysql -u root -p -e "show databases;"
+		mysql -u root -p${rootpasswd} -e "CREATE DATABASE ${dbname};"
 		echo ""
-		echo "You're good now :)"
+		echo "Database successfully created!"
+		echo ""
+		echo "Showing existing databases..."
+		mysql -u root -p${rootpasswd} -e "show databases;"
+		echo ""
 	fi
 fi
 

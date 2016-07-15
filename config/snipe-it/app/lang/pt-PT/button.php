0cfe6f7885e9fb3581fd59f722b02fac37872477
root <root@nenenui.cfht.hawaii.edu>
2016-07-14 16:51:39 -1000

    stuff again

---
+++ config/snipe-it/app/lang/pt-PT/button.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+    'actions' 	                => 'Ações',
+    'add'    	                => 'Adicionar Novo',
+    'cancel'                    => 'Cancelar',
+    'checkin_and_delete'  	    => 'Checkin & Delete User',
+    'delete'  	                => 'Remover',
+    'edit'    	                => 'Editar',
+    'restore' 	                => 'Restaurar',
+    'request'                   => 'Solicitar',
+    'submit'  	                => 'Submeter',
+    'upload'                    => 'Carregar',
+
+);

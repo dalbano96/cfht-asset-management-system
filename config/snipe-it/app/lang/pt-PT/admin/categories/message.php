0626b470ff60edfedf0a2b155de41bf691a0e5ad
root <root@nenenui.cfht.hawaii.edu>
2016-07-14 14:52:51 -1000

    Added snipe-it

---
+++ config/snipe-it/app/lang/pt-PT/admin/categories/message.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+
+    'does_not_exist' => 'A categoria não existe.',
+    'assoc_models'	 => 'This category is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this category and try again. ',
+    'assoc_items'	 => 'This category is currently associated with at least one :asset_type and cannot be deleted. Please update your :asset_type  to no longer reference this category and try again. ',
+
+    'create' => array(
+        'error'   => 'A categoria não foi criada, por favor tenta novamente.',
+        'success' => 'A categoria foi criada com sucesso.'
+    ),
+
+    'update' => array(
+        'error'   => 'A categoria não foi actualizada, por favor tenta novamente',
+        'success' => 'A categoria foi actualizada com sucesso.'
+    ),
+
+    'delete' => array(
+        'confirm'   => 'Tens a certeza que queres eliminar esta categoria?',
+        'error'   => 'Houve um problema a eliminar a categoria. Por favor tenta novamente.',
+        'success' => 'A categoria foi eliminada com sucesso.'
+    )
+
+);

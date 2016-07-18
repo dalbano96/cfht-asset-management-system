48a26cbfb4bb7c026c17cb74c6f96a6f46167d46
Daryl Albano <dalbano96@gmail.com>
2016-07-18 09:18:52 -1000

    Importing another database requires original app key bruh

--- install.sh
+++ install.sh
@@ -73,25 +73,6 @@ done
 
 if [[ $input = "y" ||  $input = "Y" ]]
 then
-	echo "Importing database..."
-	cd /home/snipeit-user/snipe-it/app/storage/dumps
-
-	prompt="Please select a file:"
-	options=( $(find -maxdepth 1 -print0 | xargs -0) )
-
-	PS3="$prompt "
-	select opt in "${options[@]}" "Quit"; 
-	do
-    		if (( REPLY == 1 + ${#options[@]} )) ; 
-		then exit
-    		elif (( REPLY > 0 && REPLY <= ${#options[@]} )); 
-		then echo  "You picked $opt which is file $REPLY"
-      		break
-    		else echo "Invalid option. Try another one."
-    		fi
-	done
-	ls -ld $opt
-	unzip -d $opt
 	
 
 ########################################################
--- test_scripts/testimport.sh
+++ test_scripts/testimport.sh
@@ -2,6 +2,7 @@
 sudo yum -y install unzip
 cd /home/snipeit-user/snipe-it/app/storage/dumps
 
+# User selects file
 prompt="Please select a file:"
 options=( $(find -maxdepth 1 -print0 | xargs -0) )
 
@@ -18,8 +19,15 @@ select opt in "${options[@]}" "Quit" ; do
         echo "Invalid option. Try another one."
     fi
 done    
-
 ls -ld $opt
-unzip $opt > /root/cfht_asset_management/test_scripts/selectedout.txt
 
-mysql -u root -p whatislife < /home/snipeit-user/snipe-it/app/storage/dumps/database/${unopt}
+# Unzip file and store output as txt file
+unzip $opt > /root/cfht_asset_management/test_scripts/output.txt
+echo break
+
+# Retrieve unzipped filename
+mysqlfile=`awk -F '/' 'NR==2 {print $2}' /root/cfht_asset_management/test_scripts/output.txt`
+
+# Import into MySQL and Snipe-IT
+mysql -u root -p whatislife < /home/snipeit-user/snipe-it/app/storage/dumps/database/${mysqlfile}
+

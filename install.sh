4624d9b70821f11323469dce0d9ac53f051743f6
Daryl Albano <dalbano96@gmail.com>
2016-07-18 10:19:47 -1000

    More stupid bugs

--- install.sh
+++ install.sh
@@ -107,21 +107,23 @@ then
 	# Import database and set app key
 	sudo yum -y install unzip
 	cd /home/${defuser}/snipe-it/app/storage/dumps
-	
+		
 	# User selects file
 	prompt="Please select a file:"
 	options=( $(find -maxdepth 1 -print0 | xargs -0) )
+
 	PS3="$prompt "
 	select opt in "${options[@]}" "Quit" ; do
-		if (( REPLY == 1 + ${#options[@]} ));
-		then exit
-		elif (( REPLY > 0 && REPLY <= ${options[@]} ));
-		then
-			echo "You picked $opt which is file $REPLY"
-			break
-		else
-			echo "Invalid option. Try another one."
-		fi
+    		if (( REPLY == 1 + ${#options[@]} )) ; then
+        		exit
+
+    		elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+        		echo  "You picked $opt which is file $REPLY"
+        		break
+	
+    		else
+       		 	echo "Invalid option. Try another one."
+	    	fi
 	done
 	ls -ld $opt	
 
